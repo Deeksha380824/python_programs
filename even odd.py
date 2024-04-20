e4d9382c54763dc3ae860a896838959fa1c510c2
Deeksha380824 <deekshabudhlakoti44@gmail.com>
2024-04-20 21:31:50 +0530

    number

---
+++ even odd.py
@@ -0,0 +1,9 @@
+num=int (input("enter the number"))
+if num<100:
+    print("number is smaller then 100")
+elif num>=100:
+    if num%2==0:
+        print("number is even")
+    elif num%2!=0:
+        print("number is odd")    
+
